81b001799238f2645e7446052330226f7871d862
rivaldo-maciel <rivaldomaciel@hotmail.com>
2022-10-15 14:16:23 -0300

    fix: fix menu responsiveness

--- src/components/MenuLinks/style.ts
+++ src/components/MenuLinks/style.ts
@@ -6,12 +6,16 @@ export const Container = styled.ul<{ device: Devices }>`
     list-style: none;
     height: 80%;
     padding: 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    align-items: center;
   }
 
   & a {
     text-decoration: none;
-    font-size: 2rem;
     color: #fff;
+    font-size: 2rem;
   }
 
   & li {
@@ -20,21 +24,17 @@ export const Container = styled.ul<{ device: Devices }>`
     align-items: center;
   }
 
-  @media ${props => props.device.tablet} {
-    display: flex;
-    flex-direction: column;
-    justify-content: space-evenly;
+  @media ${props => props.device.laptop } {
     align-items: center;
     height: 12rem;
+
+    & a {
+      font-size: 0.8rem;
+      color: ${ props => props.theme.colors.gray}
+    }
   }
 
   & li {
     margin-top: 0;
   }
-
-  & a {
-    font-size: 0.8rem;
-    color: ${ props => props.theme.colors.gray}
-  }
-
 `
